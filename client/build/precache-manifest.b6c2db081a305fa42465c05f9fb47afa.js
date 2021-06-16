47cac4b0ff2434e9df16f7ac017a30bde426fabc
Ganesh Sindagi <ganeshsindagi7@gmail.com>
2021-06-16 22:32:00 +0530

    Add files via upload

---
+++ client/build/precache-manifest.b6c2db081a305fa42465c05f9fb47afa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330c9393df44e63fea5302994b86d49e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f155e0cb9fa4af5ad132",
+    "url": "/static/css/main.2ac95ae4.chunk.css"
+  },
+  {
+    "revision": "2cd902628235f4861f0c",
+    "url": "/static/js/2.a9ed7cc1.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.a9ed7cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f155e0cb9fa4af5ad132",
+    "url": "/static/js/main.0791c866.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
